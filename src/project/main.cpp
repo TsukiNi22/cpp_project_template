89a723aae7e5aa3856a35e1a5890bcce7b376929
Tsukini <xartania.contact@gmail.com>
2026-07-06 11:38:13 +0200

    fea(ci-ccache): add usage of cahche during compilation

--- src/project/main.cpp
+++ src/project/main.cpp
@@ -1,6 +1,6 @@
 /**************************************************************\
 Edition:
-##  @date 04/06/2026 by @author Tsukini
+##  @date 06/07/2026 by @author Tsukini
 
 File Name:
 ##  @file main.cpp
